7eb292f214c5536d6c3b645143de5b3da694031e
Dzmitry Lishko <d.lishko@godeltech.com>
2023-02-26 14:30:52 +0100

    iterator

--- Iterator/exercise.ts
+++ Iterator/exercise.ts
@@ -1,3 +1,23 @@
-export class Fibonacci implements IterableIterator<number> {
-    // @todo: here the code to implement
-}
+export class Fibonacci implements Iterable<number> {
+    constructor(private length: number) {}
+    
+    [Symbol.iterator](): Iterator<number> {
+      let current = 0;
+      let next = 1;
+      let index = 0;
+  
+      return {
+        next: (): IteratorResult<number> => {
+          if (index < this.length) {
+            const value = current;
+            [current, next] = [next, current + next];
+            index++;
+            return { value, done: false };
+          } else {
+            return { value: null as any, done: true };
+          }
+        }
+      };
+    }
+  }
+  
