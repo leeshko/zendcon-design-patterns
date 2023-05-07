610ee37091fd1be8a5f8b8a4ffd35d3c7423482b
leeshko <pumpkin@tut.by>
2023-05-07 12:13:59 +0200

    add Mapper

--- Mapper/exercise.ts
+++ Mapper/exercise.ts
@@ -22,4 +22,22 @@ export interface DBRecord {
 
 export class ProductMapper {
     // @todo: here the code to implement
+    public toProduct(testData: DBRecord): Product {
+        return new Product(
+            testData.name,
+            `${testData.price}`,
+            new Manufacturer(
+                testData.manufacturerName,
+                new URL(testData.manufacturerUrl)
+            )
+        )
+    }
+    public toDBData(product: Product){
+        return {
+            name: product.name,
+            price: Number(product.price),
+            manufacturerName: product.manufacturer.name, 
+            manufacturerUrl: product.manufacturer.url.href
+        } 
+    }
 }
